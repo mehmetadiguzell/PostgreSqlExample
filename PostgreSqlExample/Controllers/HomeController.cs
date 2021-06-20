f78869368cb582ca0b11a7daf44b9f5501f8b1e8
mehmetadiguzell <76685004+mehmetadiguzell@users.noreply.github.com>
2021-06-20 21:49:58 +0300

    PostgreSql EF6
    PostgreSql EF6 Asp.Net Core ile CRUD İşlemleri

---
+++ PostgreSqlExample/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostgreSqlExample.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostgreSqlExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppContext _context;
+        public HomeController(AppContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Students.ToList();
+            return View(model);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var model = _context.Students.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Student student)
+        {        
+            _context.Entry(student).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var model = _context.Students.Find(id);
+            _context.Students.Remove(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ PostgreSqlExample/Models/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostgreSqlExample.Models
+{
+    public class AppContext:DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options):base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
